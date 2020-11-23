3fc6ee226f154d8cd3369065135134f9e5b1bf11
ericcc12 <69977966+ericcc12@users.noreply.github.com>
2020-11-23 15:58:31 -0300

    projeto de impar e par...

---
+++ src/impar_par/Impar_par.java
@@ -0,0 +1,30 @@
+package impar_par;
+
+public class Impar_par {
+
+    public static void main(String[] args)
+    {  
+     // descubra quais são os n° de 1000 a 1500 que são impares ou pares.   
+     double num, result; 
+          
+     int ini = 1000;
+     int fim = 1500;
+     System.out.println("Os respectivos número são:");   
+     
+     for(int i = 1000; i <= fim ; i++)
+     {
+      ini = 1+ini; 
+      result = ini % 2;
+      
+      if(result == 0)
+      {
+      System.out.println(i + ": " +" Par");
+      }
+      else
+      {
+      System.out.println(i + ": " +" Impar");
+      }
+     }
+    }
+    
+}
